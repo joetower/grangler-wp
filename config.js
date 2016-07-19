6135503f43c939e5b0afb0552eaa6ddab23db894
Joe Tower <joetower@gmail.com>
2016-07-18 20:45:58 -0500

    Updated icons

--- config.js
+++ config.js
@@ -26,48 +26,22 @@ module.exports = {
     colors: {
 
       // Primary
-      blue: '#51C6D8',
-      blueLightest: '#DAF6FB',
-      blueLighter: '#99E8F4',
-      blueLight: '#5ADAEE',
-      blueMidDark: '#1998AB',
-      blueDark: '#126D7A',
-      yellow: '#F3E442',
-      yellowMidDark: '#E0D23C',
+      orange: '#E06D06',
+      brown: '#433E0E',
+      cream: '#DFD6A7',
+      green: '#64A300',
 
-      // Grays and Neutrals
-      gray: '#4A4A4A',
-      grayLight: '#999999',
-      grayLighter: '#B2B2B2',
-      grayLightest: '#F4F4F4',
-      grayMid: '#4C4C4C',
-      grayDark: '#333333',
-
+      gray: '#393E41',
       white: '#FFFFFF',
       black: '#000',
 
     },
     compressPNG: true,
     customselectors: {
-      'check': ['.check li'],
-      'check-black': ['.messages.success, .messages.status'],
-      'check-white': ['.callout .check li'],
       'check_box_outline_blank': ['div:not(#foo) input[type="checkbox"] + label'],
       'check_box-black': ['div:not(#foo) input[type="checkbox"]:checked + label'],
-      'error-white': ['.messages.error'],
-      'expand_more-black': ['.submenu-icon'],
-      'expand_more-blueDark': ['.sub-menu li.menu-item--collapsed > a'],
-      'expand_more-white': ['.sub-menu li.menu-item--collapsed > a:hover, .sub-menu li.menu-item--expanded > a, .sub-menu li.menu-item--expanded > a:hover'],
-      'expand_less-white': ['.sub-menu li.menu-item--expanded > a.is-active, .sub-menu li.menu-item--expanded > a.is-active, .sub-menu li.menu-item--expanded > a.is-active:hover, .menu-open .submenu-icon'],
-      'format_quote-yellowMidDark': ['blockquote'],
-      'radio_button_unchecked': ['div:not(#foo) input[type="radio"] + label'],
-      'radio_button_checked-black': ['div:not(#foo) input[type="radio"]:checked + label'],
+      'format_quote-orange': ['blockquote'],
       'search-gray': ['input.form-search'],
-      'warning-black': ['.messages.warning'],
-      'facebook-white': ['.facebook'],
-      'instagram-white': ['.instagram'],
-      'pinterest-white': ['.pinterest'],
-      'twitter-white': ['.twitter'],
     },
   }
 };
