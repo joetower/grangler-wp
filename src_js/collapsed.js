153933f8dd53aa4f5ef60da6729177af79d6bffc
Joe Tower <joetower@gmail.com>
2016-08-09 13:24:29 -0500

    Added touchstart to get the disqus load button firing on mobile

--- src_js/collapsed.js
+++ src_js/collapsed.js
@@ -6,13 +6,13 @@
 (function ($) {
   'use strict';
   $(document).ready(function () {
-    $('.show-comments').on('click', function(){
+    $('.show-comments').on('click touchstart', function(){
     var disqus_shortname = 'joetower'; // Replace this value with *your* username.
     // ajax request to load the disqus javascript
     $.ajax({
-                      type: "GET",
-                      url: "http://" + disqus_shortname + ".disqus.com/embed.js",
-                      dataType: "script",
+                      type: 'GET',
+                      url: 'https://' + disqus_shortname + '.disqus.com/embed.js',
+                      dataType: 'script',
                       cache: true
               });
     // hide the button once comments load
